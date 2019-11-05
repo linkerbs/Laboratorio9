b4b9dc2d15f4b04bd770d5c508c00fb1d1dd285c
linkerbs <00002918@uca.edu.sv>
2019-11-04 21:00:10 -0600

    Avance

---
+++ laboratorio/src/student/Student.js
@@ -0,0 +1,18 @@
+class Student {
+    constructor(carnet, horario, tarde) {
+        this._carnet = carnet;
+        this._horario = horario;
+        this._tarde = tarde;
+    }
+
+    get carnet() { return this._carnet }
+    get horario() { return this._horario }
+    get tarde() { return this._tarde }
+
+   
+    set carnet(carnet) { this._carnet = carnet }
+    set horario(name) { this._horario = horario }
+    set tarde(tarde) { this._tarde = tarde }
+}
+
+export default Student;
